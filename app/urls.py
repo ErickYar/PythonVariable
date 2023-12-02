380f626fc8a21af377e2dbd6d35504fcc063b681
ErickYar <erickyb1985@gmail.com>
2023-12-02 14:35:19 -0500

    utizando variables y condicional en la pagina Equipo

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from app import views
 urlpatterns = [
- path('',views.home,name=''),
+ path('home/',views.home,name='home'),
+ path('inicio/',views.inicio,name='inicio'),
+ path('index/<str:nombre>',views.index,name='index'),
+ path('hincha/<int:num>',views.equipo,name='hincha')
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('Estoy aprendiendo desde django')
+
+def inicio(request):
+    return render(request,'app/inicio.html')
+
+def index(request,nombre):
+    contexto={'nombre':nombre}
+    return render(request,'app/index.html',contexto)
+
+def equipo(request,num):
+    contexto={'numero':num}
+    return render(request,'app/equipo.html',contexto)
